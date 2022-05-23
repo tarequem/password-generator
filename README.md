9b6413d75370365dd7a1b59d00abdd68f9c444e9
tarequem <moatartareque@gmail.com>
2022-05-23 18:16:15 -0400

    program will now end function if cancel is selected at the start

---
+++ README.md
@@ -0,0 +1,15 @@
+# Password Generator
+
+#URL for GitHub repository and live URL
+
+* https://github.com/tarequem/password-generator
+* https://tarequem.github.io/password-generator/
+
+# Screenshot of site
+![screenshot](/snapshot.png)
+
+# About
+* This page was designed and programmed for the purpose of generating passwords. The user will first be prompted to choose a password length, and then choose which character types they wish to have in their password. 
+
+# Contributors
+* Tareque M
--- assets/js/script.js
+++ assets/js/script.js
@@ -23,10 +23,16 @@ function generatePassword() {
 //the value of the resulting password
   var userPassword = "";
   var confirmLength = window.prompt("Please choose a length of at least 8 characters and no more than 128 characters.");
+    //breaks out of function to prevent new prompt.
+    if (confirmLength === null) {
+      return ""; 
+    }
+    //isNaN checks if a non-numeral value was inputed 
     if (isNaN(confirmLength)) {
       alert("Please enter a numerical value");
       return generatePassword();
     }
+    //ensures paremeters are between 8 and 128
     if (confirmLength < 8 || confirmLength > 128) {
       alert("Please choose a length of at least 8 characters and no more than 128 characters.");
       return generatePassword();
@@ -42,26 +48,26 @@ function generatePassword() {
     alert("Choose at least one character type!");
     return generatePassword();
   }
+
 //if statements to connect confirm windows to global variables 
   if(confirmLower){
     characters += lower;
   }
-  
   if(confirmUpper){
     characters += upper;
   }
-  
   if(confirmNumber){
     characters += number;
   }
-  
   if(confirmSpecial){
     characters += special;
   }
+  
 //combines characters to form userPassword 
   for (var i = 0; i < confirmLength; i++ ) {
     userPassword += characters.charAt(Math.floor(Math.random() * characters.length));
   }
+  
 //returns password to generatepassword 
   return userPassword;
 };
