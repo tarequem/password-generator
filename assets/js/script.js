924da9d6c27dfaa67942370e12c500cb55b4ec23
tarequem <moatartareque@gmail.com>
2022-05-23 17:48:44 -0400

    defined global variables, made choose password length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,44 @@
-// Assignment code here
-
-
-// Get references to the #generate element
-var confirmLength;
-var confirmUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var confirmLower = "abcdefghijklmnopqrstuvwxyz";
-var confirmNumber = "0123456789";
-var confirmSpecial = "~`! @#$%^&*()_-+={[}]|\\\:\";'<,>.?/";
-
-//prompts that will ask the user what characters they want in their password after clicking button
 var generateBtn = document.querySelector("#generate");
-function generatePassword() {
-  console.log("Generate")
-  //prompt user for confirming length
-  //confirm lowercase
-  //confirm uppercase 
-  //confirm number
-  //isNaN() to determine whether a number is being used
-  //confirm special
-  //!variables to check if not true/alerting for correction
-  //if statement - alert they need to choose at least one character
-  //charAt to return characters from chosen index 
-  return password;
-};
+//global variables/arrays
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var special = "!@#$%^&*(){}<>?:+_-/\||~";
 
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+};
 
-}
-
-// Get references to the #generate element
-
-
-//Variables for password creation
-
-
-// Add event listener to generate button
+//event listener, to be called on within functions
 generateBtn.addEventListener("click", writePassword);
 
+//generate password function
+function generatePassword() {
 
-//function generating password writePassword()
+//characters combines global variables under one local variable
+  var characters = "";
+//the value of the resulting password
+  var userPassword = "";
+  var confirmLength = window.prompt("Please choose a length of at least 8 characters and no more than 128 characters.");
+    if (isNaN(confirmLength)) {
+      alert("Please enter a numerical value");
+      return generatePassword();
+    }
+    if (confirmLength < 8 || confirmLength > 128) {
+      alert("Please choose a length of at least 8 characters and no more than 128 characters.");
+      return generatePassword();
+    }
+//confirm windows for character sets user wants
 
+//alert if user denied all character sets
 
-//random math generator, picking randomly from each array (letters, numbers, special characters) to make password at specified length. 
+//if statements to connect confirm windows to global variables 
+
+//combines characters to form userPassword 
+
+//returns password to generatepassword 
+  return userPassword;
+};
